5196a47aac5a7b1d8299ec9218fbd569c88ad7bd
Natalie Lipps <natalie.lipps@stud.th-deg.de>
2020-06-23 11:16:59 +0200

    Rename

--- fahrtenbuch/src/main/java/de/thd/pms/AufgabenmanagementApplication.java
+++ fahrtenbuch/src/main/java/de/thd/pms/AufgabenmanagementApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FahrtenbuchApplication {
+public class Aufgabenmanagement#hApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FahrtenbuchApplication.class, args);
+		SpringApplication.run(AufgabenmanagementhApplication.class, args);
 	}
 }
